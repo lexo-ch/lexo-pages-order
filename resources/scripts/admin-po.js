b9fcc6193e0223f83c3f680988ae30836fdaf7ec
Miljan Puzović <mpuzovic@lexo.ch>
2025-04-16 10:54:30 +0200

    fix(core): Prevent updating subpages if the order is not changed

--- resources/scripts/admin-po.js
+++ resources/scripts/admin-po.js
@@ -2,15 +2,28 @@
  * Admin .js file
  */
 
-jQuery(document).ready(function($) {
-    $("#sortable-subpages").sortable({
-        axis: "y",
-        opacity: 0.7,
-        update: function(event, ui) {
-            const order = $(this).find('li').map(function() {
-                return $(this).data('id');
-            }).get().join(',');
-            $("#subpage_order").val(order); 
-        }
-    }).disableSelection();
+document.addEventListener('DOMContentLoaded', function () {
+  const sortableList = document.getElementById('po-sortable-subpages');
+
+  if (!sortableList) {
+    return;
+  }
+
+  const orderInput = document.getElementById('po-subpage-order');
+
+  const initialItems = Array.from(sortableList.querySelectorAll('li'));
+  const initialOrder = initialItems.map(item => item.dataset.id).join(',');
+
+  $(sortableList).sortable({
+    axis: 'y',
+    opacity: 0.7,
+    update: function (event, ui) {
+      const items = Array.from(sortableList.querySelectorAll('li'));
+      const currentOrder = items.map(item => item.dataset.id).join(',');
+
+      orderInput.value = currentOrder === initialOrder
+        ? ''
+        : currentOrder;
+    }
+  }).disableSelection();
 });
