e24c8a2119136129b0f7f813e3336d9fb16b40d0
Miljan Puzović <mpuzovic@lexo.ch>
2025-04-16 11:10:28 +0200

    fix(core): Optimisation & refactoring

--- src/Core/Plugin/Order.php
+++ src/Core/Plugin/Order.php
@@ -2,29 +2,24 @@
 
 namespace LEXO\PO\Core\Plugin;
 
+use WP_Post;
+
+use const LEXO\PO\{
+    DOMAIN
+};
+
 class Order
 {
-    private $renderWhiteList = [
-        'page',
-    ];
-
     public function __construct()
     {
         add_action('add_meta_boxes', [$this, 'renderMetabox']);
     }
 
-    public function renderMetabox() : void
+    public function renderMetabox(): void
     {
-        $whitelist = $this->renderWhiteList;
-        $screen = get_current_screen();
-    
-        if (!in_array($screen->post_type, $whitelist)) {
-            return;
-        }
-    
         add_meta_box(
             'subpage_settings_box',
-            'Subpage Settings',
+            __('Subpage Settings', 'po'),
             [self::class, 'renderMetaboxContent'],
             'page',
             'normal',
@@ -32,99 +27,131 @@ class Order
         );
     }
 
-    public static function renderMetaboxContent($post) : void
+    public static function renderMetaboxContent(WP_Post $post): void
     {
-        if (!current_user_can('edit_posts')) { 
-            return;
-        }
-    
         if (self::isHomepage($post)) {
-            ?>
-                <p><?php echo __('Subpage settings are disabled for this page.', 'po'); ?></p>
-                <p><?php echo __('If you want to change the structure or order of subpages, you can do so ', 'po'); ?><a href="<?php echo admin_url('nav-menus.php'); ?>" target><?php echo __('here.', 'po'); ?></a>.</p>
-            <?php 
-            
+            echo self::getHomepageMessage();
             return;
         }
-        
+
         self::displaySubpageSortingBox($post);
         self::displayExclusionBox($post);
         self::addHiddenFields();
     }
 
-    public static function displaySubpageSortingBox($post) : void
+    public static function getHomepageMessage(): string
+    {
+        ob_start(); ?>
+            <p><?php echo __('Subpage settings are disabled for this page because its subpages are the part of the main menu.', 'po'); ?></p>
+            <p>
+                <?php echo sprintf(
+                    __('If you want to change the order of the pages in the main menu, you can do so %s.', 'po'),
+                    '<a href="' . admin_url('nav-menus.php') . '" target="_blank">' . __('here', 'po') . '</a>'
+                ); ?>
+            </p>
+        <?php return ob_get_clean();
+    }
+
+    public static function displaySubpageSortingBox(WP_Post $post): void
     {
         $subpages = get_pages([
             'parent' => $post->ID,
             'sort_column' => 'menu_order',
             'sort_order' => 'ASC',
         ]);
-    
-        if (empty($subpages)) {
-            ?>
-                <p><?php echo __('No subpages found for this page.', 'po'); ?></p>
-            <?php
 
-            return;
-        }
-        
-        ?>
-            <ul id="sortable-subpages">
-                <?php
-                    foreach ($subpages as $subpage) {
-                        ?>
-                            <li class="sortable-item <?php echo self::printItemClass($subpage); ?>" data-id="<?php echo esc_attr($subpage->ID); ?>">
-                                <?php echo esc_html($subpage->post_title); ?>
-                                <a href="<?php echo get_edit_post_link($subpage->ID); ?>" target="_blank"><?php echo __('Go to page →', 'po') ?></a>
-                            </li>
-                        <?php
-                    }
-                ?>
-            </ul>
+        if (empty($subpages)) { ?>
+            <p><?php echo __('No subpages found for this page.', 'po'); ?></p>
+            <?php return;
+        } ?>
+
+        <ul id="po-sortable-subpages">
+            <?php foreach ($subpages as $subpage) {
+                $is_page_excluded = self::isPageExcluded($subpage);
+                $parent_excluded = self::isAnyParentExcluded($subpage);
+
+                $classes = ['po-sortable-item'];
+
+                if ($is_page_excluded || $parent_excluded) {
+                    $classes[] = 'excluded';
+                } ?>
+
+                <li
+                    class="<?php echo implode(' ', $classes); ?>"
+                    data-id="<?php echo esc_attr($subpage->ID); ?>"
+                    <?php if ($is_page_excluded) { ?>
+                        title="<?php echo __('This page and all it\'s subpages are excluded from the main menu.', 'po'); ?>"
+                    <?php } elseif ($parent_excluded !== false) {
+                        $title = sprintf(
+                            __('This page and all it\'s subpages are excluded from the main menu because its parent page %s is excluded.', 'po'),
+                            get_the_title($parent_excluded)
+                        ); ?>
+                        title="<?php echo $title; ?>"
+                    <?php } ?>
+                >
+                    <div class="page-title"><?php echo esc_html($subpage->post_title); ?></div>
+                    <div class="controls">
+                        <a
+                            class="po-sortable-url"
+                            href="<?php echo get_edit_post_link($subpage->ID); ?>"
+                            target="_blank"
+                        >&rightarrow;</span></a>
+                    </div>
+                </li>
+            <?php } ?>
+        </ul>
+        <small class="post-attributes-help-text">
+            <?php echo __('Change the order of the subpages by clicking and dragging the respective page to the desired location.', 'po'); ?>
+        </small>
         <?php
     }
-    
-    public static function displayExclusionBox($post) : void
+
+    public static function displayExclusionBox(WP_Post $post): void
     {
         if ($post->post_status != 'publish' || self::isPageInAnyMenu($post)) {
             return;
-        }
-    
-        ?>
-            <hr>
-            <div class="exclusion-box" id="exclude-box">
-                <input 
-                    type="checkbox"
-                    id="exclude-page"
-                    name="custom_exclude"
-                    data-page-id="<?php echo $post->ID; ?>"
-                    <?php if (self::isAnyParentExcluded($post)) { ?>
-                        disabled
-                        checked
-                    <?php } else {
-                        echo get_post_meta($post->ID, 'custom_exclude', true) ? 'checked' : '';
-                        } ?>
+        } ?>
+
+        <div id="po-exclusion-box">
+            <?php $parent_excluded = self::isAnyParentExcluded($post);
+
+            if ($parent_excluded !== false) {
+                echo sprintf(
+                    __('This page and all it\'s subpages are excluded from the main menu because its parent page %s is excluded.', 'po'),
+                    '<a href="' . get_edit_post_link($parent_excluded) . '" target="_blank">' . get_the_title($parent_excluded) . '</a>'
+                );
+            } else { ?>
+                <label
+                    for="po-exclude-page"
                 >
-                <label for="exclude-page"><?php echo __('Exclude page from menu', 'po'); ?></label>
-            </div>
-        <?php
-    }
-    
-    public static function addHiddenFields() : void
-    {
-        wp_nonce_field('subpage_settings_action', 'subpage_settings_nonce');
-    
-        ?>
-            <input 
-                type="hidden" 
-                name="subpage_order" 
-                id="subpage_order" 
-                value=""
-            >
+                    <input
+                        type="checkbox"
+                        id="po-exclude-page"
+                        name="custom_exclude"
+                        data-page-id="<?php echo $post->ID; ?>"
+                        <?php checked(self::isPageExcluded($post), true, true); ?>
+                    />
+                    <?php echo __('Exclude page from menu<br><small>(and all it\'s subpages)</small>', 'po'); ?>
+                </label>
+            <?php } ?>
+        </div>
         <?php
     }
 
-    public static function isPageInAnyMenu($post) : bool
+    public static function addHiddenFields(): void
+    {
+        wp_nonce_field('subpage_settings_action', 'subpage_settings_nonce'); ?>
+
+        <input
+            type="hidden"
+            name="po-subpage-order"
+            id="po-subpage-order"
+            value=""
+        >
+        <?php
+    }
+
+    public static function isPageInAnyMenu(WP_Post $post): bool
     {
         $menus = wp_get_nav_menus();
 
@@ -138,14 +165,16 @@ class Order
         return false;
     }
 
-    public static function printItemClass($post) : string
+    public static function isPageExcluded($post): bool
     {
-        return (get_post_meta($post->ID, 'custom_exclude', true) || $post->post_status != 'publish') 
-            ? 'excluded' 
-            : '';
+        if (!($post instanceof WP_Post)) {
+            $post = get_post($post);
+        }
+
+        return get_post_meta($post->ID, 'custom_exclude', true) || $post->post_status != 'publish';
     }
 
-    public static function isHomepage($post) : bool
+    public static function isHomepage(WP_Post $post): bool
     {
         if (self::isMlAdminPageRegistered("lexo-ml-core.php") && empty($post->post_parent)) {
             return true;
@@ -154,108 +183,120 @@ class Order
         return get_option('page_on_front') === $post->ID;
     }
 
-    public static function isMlAdminPageRegistered($slug) : bool
+    public static function isMlAdminPageRegistered(string $slug): bool
     {
         global $menu, $submenu;
-        
-        foreach ($menu as $item) if (isset($item[2]) && $item[2] === $slug) return true;
-        foreach ($submenu as $items) foreach ($items as $item) if (isset($item[2]) && $item[2] === $slug) return true;
 
-        return false;
-    }
-
-    public static function isAnyParentExcluded($post) : bool
-    {
-        $ancestors = get_post_ancestors($post->ID);
-
-        foreach ($ancestors as $ancestor) {
-            if (get_post_meta($ancestor, 'custom_exclude', true)) {
+        foreach ($menu as $item) {
+            if (isset($item[2]) && $item[2] === $slug) {
                 return true;
+            }
+        }
+
+        foreach ($submenu as $items) {
+            foreach ($items as $item) {
+                if (isset($item[2]) && $item[2] === $slug) {
+                    return true;
+                }
             }
         }
 
         return false;
     }
 
-    public function setNewPageMenuOrder($new_status, $old_status, $post) : void
+    public static function isAnyParentExcluded(WP_Post $post)
     {
-        remove_action('transition_post_status', [$this, 'setNewPageMenuOrder'], 10);
+        $ancestors = get_post_ancestors($post->ID);
+
+        foreach ($ancestors as $ancestor) {
+            if (self::isPageExcluded($ancestor)) {
+                return $ancestor;
+            }
+        }
+
+        return false;
+    }
+
+    public function setMenuOrderForNewPage(string $new_status, string $old_status, WP_Post $post): void
+    {
+        remove_action('transition_post_status', [$this, 'setMenuOrderForNewPage'], 10);
 
         if ($post->post_type !== 'page' || $new_status !== 'publish' || $old_status === 'publish') {
             return;
         }
-    
+
         if (get_post_meta($post->ID, '_menu_order_set', true)) {
             return;
         }
-            
+
         $siblings = get_pages([
             'parent' => $post->post_parent,
             'post_status' => 'publish',
             'hierarchical' => false,
             'exclude' => $post->ID
         ]);
-    
+
         wp_update_post([
             'ID' => $post->ID,
             'menu_order' => count($siblings)
         ]);
-        
+
         update_post_meta($post->ID, '_menu_order_set', true);
-        add_action('transition_post_status', [$this, 'setNewPageMenuOrder'], 10, 3);
+        add_action('transition_post_status', [$this, 'setMenuOrderForNewPage'], 10, 3);
     }
-    
-    public function updateMenuOrderOnParentChange($post_ID, $post_after, $post_before) : void
+
+    public function updateMenuOrderOnParentChange(int $post_id, WP_Post $post_after, WP_Post $post_before): void
     {
         if ($post_after->post_type !== 'page' || $post_after->post_parent == $post_before->post_parent) {
             return;
         }
-        
+
         $siblings = get_pages([
             'parent' => $post_after->post_parent,
             'post_status' => 'publish',
             'hierarchical' => false,
-            'exclude' => $post_ID
+            'exclude' => $post_id
         ]);
-    
+
         wp_update_post([
-            'ID' => $post_ID,
+            'ID' => $post_id,
             'menu_order' => count($siblings)
         ]);
     }
 
-    public function saveSubpageSettingsData($post_id) : void
+    public function saveSubpageSettingsData(int $post_id): void
     {
+        if (!current_user_can('edit_post', $post_id)) {
+            return;
+        }
+
         remove_action('save_post', [$this, 'saveSubpageSettingsData']);
 
         if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
             return;
         }
-    
+
         if (!isset($_POST['subpage_settings_nonce'])) {
             return;
         }
-    
+
         if (!wp_verify_nonce($_POST['subpage_settings_nonce'], 'subpage_settings_action')) {
             return;
         }
-    
-        if (!current_user_can('edit_post', $post_id)) {
-            return;
-        }
-    
-        if (!empty($_POST['subpage_order'])) {
-            $order = explode(',', $_POST['subpage_order']);
-            
-            foreach ($order as $index => $page_id) {
+
+        if (!empty(trim($_POST['po-subpage-order']))) {
+            $order = explode(',', $_POST['po-subpage-order']);
+
+            foreach ($order as $index => $subpost_id) {
                 wp_update_post([
-                    'ID' => (int)$page_id,
+                    'ID' => $subpost_id,
                     'menu_order' => $index
                 ]);
             }
         }
-    
+
         $exclude = isset($_POST['custom_exclude']) ? 'on' : '';
+
         if ($exclude) {
             update_post_meta($post_id, 'custom_exclude', $exclude);
         } else {
@@ -265,23 +306,35 @@ class Order
         add_action('save_post', [$this, 'saveSubpageSettingsData']);
     }
 
-    public static function printExcludedColumn() : void
+    public static function addExcludedColumn(array $columns): array
     {
-        if (self::isAnyParentExcluded(\get_post()) || \get_post_meta(\get_the_ID(), 'custom_exclude', true)) {
-            echo '<span style="color: red; font-size: 20px;">' . __('&#10006;', 'exclude-plugin') . '</span>';
-        } else {
-            echo '<span style="color: green; font-size: 20px; vertical-align: sub;">' . __('&#10004', 'exclude-plugin') . '</span>';
-        }
-    }
+        $customColumn = [
+            'excluded_pages_column' => __('Excluded from main menu', 'po')
+        ];
 
-    public static function addExcludedColumn( $columns ) : array
-    {
-        $customColumn = array(
-            'excluded' => __('Excluded from main menu', 'po')
-        );
-
-        $columns = array_merge( $columns, $customColumn );
+        $columns = array_merge($columns, $customColumn);
 
         return $columns;
+    }
+
+    public static function printExcludedColumn(string $column_name, int $post_id): void
+    {
+        if ($column_name === 'excluded_pages_column') {
+            $post = get_post();
+
+            if (self::isAnyParentExcluded($post) !== false || self::isPageExcluded($post)) {
+                $excluded_message = apply_filters(
+                    DOMAIN . '/message/column_excluded',
+                    '<span class="excluded-page true"></span>'
+                );
+            } else {
+                $excluded_message = apply_filters(
+                    DOMAIN . '/message/column_not_excluded',
+                    '<span class="excluded-page false"></span>'
+                );
+            }
+
+            echo $excluded_message;
+        }
     }
 }
